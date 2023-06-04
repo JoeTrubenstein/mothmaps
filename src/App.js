3068d74f1860e345cfbd98832f01ad1184fb5325
Joseph Trubenstein <josephtrubenstein@gmail.com>
2023-06-04 16:11:47 +0900

    ui

--- src/App.js
+++ src/App.js
@@ -63,9 +63,10 @@ export default function App(props) {
       </div>
       {/* NavBar Area */}
 
+
       {/* Hero Area */}
       
-      <section className="text-gray-300 bg-gray-900 body-font container px-5 py-24 mx-auto">
+      <section className="text-gray-300 bg-gray-900 body-font container px-5 py-6 mx-auto">
         <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
           <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
@@ -99,11 +100,9 @@ export default function App(props) {
       </section>
 
 
-
-
       {/* Sighting Area */}
       <section id="sights" className="text-gray-300 bg-gray-900 body-font">
-        <div className="container px-5 py-24 mx-auto">
+        <div className="container px-5 py-8 mx-auto">
           <div className="flex flex-wrap -mx-4 -my-8">
             {data ? (
               <>
--- src/components/contact.js
+++ src/components/contact.js
@@ -85,7 +85,7 @@ function Contact() {
 
   return (
     <section id="contact" className="text-gray-500 bg-gray-900 body-font">
-      <div className="container px-5 py-24 mx-auto">
+      <div className="container px-5 py-8 mx-auto">
         <div className="flex flex-col text-center w-full mb-12">
           <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">
             Report a Sighting
